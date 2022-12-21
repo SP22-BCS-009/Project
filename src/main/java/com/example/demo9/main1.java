0a47b24f4533917bbaf3d95a30eb933a94c35d52
SP22-BCS-009 <marleenajmal120@gmail.com>
2022-12-21 14:31:53 -0800

    Add via Upload

---
+++ src/main/java/com/example/demo9/main1.java
@@ -0,0 +1,9 @@
+package com.example.demo9;
+
+import java.io.IOException;
+
+public class main1 {
+    public static void main(String[] args) throws IOException {
+        HelloApplication.main(args);
+    }
+}
